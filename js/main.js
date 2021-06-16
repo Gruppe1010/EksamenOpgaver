ed4fd8516b10fefe0e50473fa8972fdca643a274
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-06-16 16:26:00 +0200

    hejehej

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
+
+//p1
 function out(s) {
   console.log(s);
 }
 
 out(2);
 
+//p2
 function out2(arguments) {
   console.log(arguments);
 }
 
 out2(1, 211, 40, 5321);
 
+//p3
 out(4=="4");
 out(4>"12");
 out("4">"12");
+
+//p4
 out(4==="4");
 
+//p7
 const aa = 4 + "12";
 out(typeof aa);
 
@@ -24,5 +31,6 @@ out(bb);
 const cc = true + 4 + "hej";
 out(cc);
 
+//p8
 const dd = true + 1 + Number("25");
 out(dd);
